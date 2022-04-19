93f39a640e8a92eb5612db80a9feb495474d095a
Dalton <slattery.da@gmail.com>
2022-04-19 12:08:07 -0500

    made op crawling credits, added to home page and set up autoplay

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,27 +1,39 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import ReactPlayer from 'react-player'
 
-function Home({user}) {
+function Home({ user }) {
   return (
     <div>
-        <h1>@</h1>
-        <h3>+++The+++</h3>
-        <h3>Sticker</h3>
-        <h3>Collector</h3>
-        <h3>++++++++++</h3>        
-        {!user && <>
-          <div className='nav-button'>
-              <NavLink to='/signup'>
-              <button>Signup</button>
-              </NavLink>
-          </div>
-          <div className='nav-button'>
-              <NavLink to='/login'>
-              <button>Login</button>
-              </NavLink>
-          </div>
-        </>
-        }        
+      {/* <h1>@</h1>
+      <h3>+++The+++</h3>
+      <h3>Sticker</h3>
+      <h3>Collector</h3>
+      <h3>++++++++++</h3> */}
+      <div className='video'>
+        <ReactPlayer
+          url="https://www.dropbox.com/s/d7xzf344bp2dpby/vlc-record-2022-04-19-11h07m55s-test.mp4-.mp4?dl=0"
+          playing={true}
+          loop={true}
+          volume={0}
+          muted={true}
+          width='65%'
+          height='auto'
+        />
+      </div>
+      {!user && <>
+        <div className='nav-button'>
+          <NavLink to='/signup'>
+            <button>Signup</button>
+          </NavLink>
+        </div>
+        <div className='nav-button'>
+          <NavLink to='/login'>
+            <button>Login</button>
+          </NavLink>
+        </div>
+      </>
+      }
     </div>
   )
 }
