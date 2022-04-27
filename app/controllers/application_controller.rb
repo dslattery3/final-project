9e4952428b724ecdeb09005bf88de8e3fc5376da
Dalton <slattery.da@gmail.com>
2022-04-27 09:37:28 -0500

    setting up for heroku deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
     before_action :authorized
 
+    def home
+        render json: {message: 'Server is up!'}
+    end
+
     private
     def render_invalid(invalid)
         render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
 
   get "/me", to: "users#me"
+
+  root to: 'application#home'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
